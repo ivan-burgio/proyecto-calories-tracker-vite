01eed3658d3902dee3ca91988fcdd1a523ed7b5a
ivan-burgio <ivan.24.burgio@gmail.com>
2024-05-27 10:52:46 -0300

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
     return (
         <>
             <header className="bg-lime-600 py-3">
-                <div className="max-w-4xl mx-auto flex justify-between">
+                <div className="max-w-4xl mx-auto flex justify-between items-center">
                     <h1 className="text-center text-lg font-bold text-white uppercase">
                         Contador de Calorias
                     </h1>
--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import type { Activity } from "../types";
+import CalorieDisplay from "./CalorieDisplay";
 
 type CalorieTrackerProps = {
     activities: Activity[];
@@ -27,6 +28,11 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
         [activities]
     );
 
+    const netCalories = useMemo(
+        () => caloriesConsumed - caloriesBurned,
+        [activities]
+    );
+
     return (
         <>
             <h2 className="text-4xl font-black text-white text-center">
@@ -34,20 +40,12 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
             </h2>
 
             <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                    <span className="font-black text-6xl text-orange">
-                        {caloriesConsumed}
-                    </span>
-                    Consumidas
-                </p>
-                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-                    <span className="font-black text-6xl text-orange">
-                        {caloriesBurned}
-                    </span>
-                    Quemadas
-                </p>
-            </div>
+                <CalorieDisplay calories={caloriesConsumed} text="Consumidas" />
 
+                <CalorieDisplay calories={netCalories} text="Totales" />
+
+                <CalorieDisplay calories={caloriesBurned} text="Quemadas" />
+            </div>
         </>
     );
 }
